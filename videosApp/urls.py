0bdf0c7e04341ab41aabae886479310b059bf628
odnashestaia <odna_shestaia@mail.ru>
2024-04-20 16:22:55 +0300

    доделал вывод\удаление\деталь плейлиста чуть дополнил реадми

--- videosApp/urls.py
+++ videosApp/urls.py
@@ -1,8 +1,25 @@
 from django.urls import path
-from .views import VideoDetailView, VideoListView
+from .views import (
+    VideoDetailView,
+    VideoListView,
+    VideoDelete,
+    PlaylistDelete,
+    PlaylistDetailView,
+    playlist_list,
+    delete_video_playlist,
+)
 
 
 urlpatterns = [
     path("", VideoListView.as_view(), name="videos"),
     path("video/<int:pk>", VideoDetailView.as_view(), name="video-detail"),
+    path("video/<int:pk>/delete", VideoDelete.as_view(), name="video-delete"),
+    path("playlists", playlist_list, name="playlists"),
+    path("playlist/<int:pk>", PlaylistDetailView.as_view(), name="playlist-detail"),
+    path("playlist/delete/<int:pk>", PlaylistDelete.as_view(), name="playlist-delete"),
+    path(
+        "playlist/<int:playlist_pk>/video/delete/<int:pk>",
+        delete_video_playlist,
+        name="playlist-delete-video",
+    ),
 ]
--- videosApp/views.py
+++ videosApp/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
-from django.views.generic import ListView, DetailView
+import json
+from django.shortcuts import render, HttpResponse, get_list_or_404, redirect
+from django.urls import reverse_lazy
+from django.views.generic import ListView, DetailView, DeleteView
 from .models import Video, Playlist, Category
 
 
@@ -11,3 +13,59 @@ class VideoListView(ListView):
 class VideoDetailView(DetailView):
     template_name = "videos/video_detail.html"
     model = Video
+
+
+class VideoDelete(DeleteView):
+    model = Video
+    success_url = reverse_lazy("videos")
+    template_name = "standardForm\delete.html"
+
+
+def playlist_list(request):
+    playlists = Playlist.objects.all()
+    context = {"object_list": []}
+    for playlist in playlists:
+        video = playlist.videos.first() if playlist.videos.exists() else None
+        context["object_list"].append(
+            {
+                "pk": playlist.pk,
+                "title": playlist.title,
+                "description": playlist.description,
+                "created_at": playlist.created_at,
+                "preview": video.preview,
+            }
+        )
+    return render(request, "playlist/playlists.html", context)
+
+
+class PlaylistDetailView(DetailView):
+    model = Playlist
+    template_name = "playlist/playlist_detail.html"
+
+    def get_object(self):
+        pk = self.kwargs.get("pk")
+        playlist = Playlist.objects.get(pk=pk)
+        return playlist
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        playlist = self.get_object()
+        videos = playlist.videos.all()
+        context["videos"] = videos
+        return context
+
+
+def delete_video_playlist(request, playlist_pk, pk):
+    playlist_obj = Playlist.objects.get(pk=playlist_pk)  # получаем объект плейлиста
+
+    video_obj = Video.objects.get(pk=pk)  # получаем объект видео
+
+    playlist_obj.videos.remove(video_obj)  # удаляем видео из плейлиста
+    playlist_obj.save()  # сохраняем изменения в базе данных
+    return redirect(reverse_lazy("playlist_list"))
+
+
+class PlaylistDelete(DeleteView):
+    model = Playlist
+    success_url = reverse_lazy("playlists")
+    template_name = "standardForm\delete.html"
