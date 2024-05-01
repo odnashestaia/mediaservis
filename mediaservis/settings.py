ae69653ecd41d18775a55d77595827083a13ce90
odnashestaia <odna_shestaia@mail.ru>
2024-05-01 12:05:08 +0300

    поменял настройки, сохраняю env файл

--- mediaservis/settings.py
+++ mediaservis/settings.py
@@ -21,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-38$%(%a)i@p=+%7&-&@)-5!!jxrsjy!5^0qry_9#ki_@04x025"
+SECRET_KEY = "django-insecure-38$%(%a)i@p=+%7&-&@)-5!!jxrsjy!5^0qry_9#ki_@04x025"  #
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = True  #
 
 ALLOWED_HOSTS = []
 
@@ -121,9 +121,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = "en-us"
+LANGUAGE_CODE = "RU"
 
-TIME_ZONE = "UTC"
+TIME_ZONE = "Europe/Moscow"
 
 USE_I18N = True
 
